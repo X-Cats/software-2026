c8c63ce52738791333ddfde2199c5a8bea155147
Noor <thatoneguy2040@gmail.com>
2026-02-11 18:09:07 -0500

    Create Intake Subsystem (#4)
    * Test Commit
    
    * Test Commit 2
    
    * started intake stuff
    
    * started intake more intake stuff
    
    * started intake stuff and implemented sim suff
    
    * resolved comments
    
    * added some comments
    
    ---------
    
    Co-authored-by: Aidan Sciortino <a.l.f.sciorti@gmail.com>
    Co-authored-by: Noor <890237139@rcsd121.org>

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems.intake;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface IntakeIO {
+  @AutoLog
+  public static class IntakeIOInputs {
+    public double rollerAppliedVolts = 0.0;
+
+    public double deployAppliedVolts = 0.0;
+  }
+
+  public default void updateInputs(IntakeIOInputs inputs) {}
+
+  /**
+   * sets the deployment motor's voltage
+   * @param volts voltage value from -12 to 12
+   */
+  public default void setDeploymentMotorVoltage(double volts) {}
+  /**
+   * sets the roller motor's voltage
+   * @param volts voltage value from -12 to 12
+   */
+  public default void setRollerMotorVoltage(double volts) {}
+}
