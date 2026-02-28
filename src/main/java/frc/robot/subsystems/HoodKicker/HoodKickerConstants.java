7af1ede0349a4d1d383776c5babda5e3a47324d1
Aidan Sciortino <a.l.f.sciorti@gmail.com>
2026-02-28 09:52:08 -0500

    Plug in motor IDs

--- src/main/java/frc/robot/subsystems/HoodKicker/HoodKickerConstants.java
+++ src/main/java/frc/robot/subsystems/HoodKicker/HoodKickerConstants.java
@@ -1,13 +1,12 @@
 package frc.robot.subsystems.HoodKicker;
 
 public class HoodKickerConstants {
-  // TODO get actual motor id
-  public static final int HOOD_MOTOR_ID = 0;
+  public static final int HOOD_MOTOR_ID = 54;
   public static final int HOOD_MOTOR_VOLTAGE = 1;
   public static final double HOOD_MOTOR_REDUCTION = 1;
   public static final double HOOD_MOTOR_CURRENT_LIMIT = 1;
 
-  public static final int KICKER_MOTOR_ID = 1;
+  public static final int KICKER_MOTOR_ID = 55;
   public static final int KICKER_MOTOR_VOLTAGE = 1;
   public static final double KICKER_MOTOR_REDUCTION = 1;
   public static final double KICKER_MOTOR_CURRENT_LIMIT = 1;
