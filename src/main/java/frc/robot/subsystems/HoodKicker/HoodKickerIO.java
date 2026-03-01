3082eccf3513f8b6be686560ba19fdea4acf74a6
Aidan Sciortino <a.l.f.sciorti@gmail.com>
2026-03-01 16:25:47 -0500

    Add appropriate status signals to hood

--- src/main/java/frc/robot/subsystems/HoodKicker/HoodKickerIO.java
+++ src/main/java/frc/robot/subsystems/HoodKicker/HoodKickerIO.java
@@ -6,8 +6,15 @@ public interface HoodKickerIO {
   @AutoLog
   public static class HoodIOInputs {
     public double hoodAppliedVolts = 0.0;
+    public double hoodVelocity = 0.0;
     public double hoodPosition = 0.0;
+    public double hoodTorqueCurrent = 0.0;
+    public double hoodSupplyCurrent = 0.0;
+
     public double kickerAppliedVolts = 0.0;
+    public double kickerVelocity = 0.0;
+    public double kickerTorqueCurrent = 0.0;
+    public double kickerSupplyCurrent = 0.0;
   }
 
   public default void updateInputs(HoodIOInputs inputs) {}
--- src/main/java/frc/robot/subsystems/HoodKicker/HoodKickerIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/HoodKicker/HoodKickerIOTalonFX.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems.HoodKicker;
 
-import com.ctre.phoenix6.CANBus;
+import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
@@ -8,23 +8,35 @@ import com.ctre.phoenix6.controls.PositionTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Voltage;
 import frc.robot.subsystems.shooter.ShooterConstants;
+import frc.robot.util.PhoenixUtil;
 
 public class HoodKickerIOTalonFX implements HoodKickerIO {
-  private final TalonFX hood;
-  private final StatusSignal<Voltage> hoodAppliedVolts;
+  // Motors
+  private final TalonFX hood = new TalonFX(HoodKickerConstants.HOOD_MOTOR_ID);
   private final TalonFX kicker = new TalonFX(HoodKickerConstants.KICKER_MOTOR_ID);
-  private final StatusSignal<Voltage> kickerAppliedVolts = kicker.getMotorVoltage();
 
+  // Control Requests
   private final PositionTorqueCurrentFOC positionControl =
       new PositionTorqueCurrentFOC(0.0).withUpdateFreqHz(0.0);
 
+  // Loggables
+  private final StatusSignal<Voltage> hoodAppliedVolts = hood.getMotorVoltage();
+  private final StatusSignal<AngularVelocity> hoodVelocity = hood.getVelocity();
+  private final StatusSignal<Angle> hoodPosition = hood.getPosition();
+  private final StatusSignal<Current> hoodTorqueCurrent = hood.getTorqueCurrent();
+  private final StatusSignal<Current> hoodSupplyCurrent = hood.getSupplyCurrent();
+
+  private final StatusSignal<Voltage> kickerAppliedVolts = kicker.getMotorVoltage();
+  private final StatusSignal<AngularVelocity> kickerVelocity = kicker.getVelocity();
+  private final StatusSignal<Current> kickerTorqueCurrent = kicker.getTorqueCurrent();
+  private final StatusSignal<Current> kickerSupplyCurrent = kicker.getSupplyCurrent();
+
   public HoodKickerIOTalonFX() {
-
-    hood = new TalonFX(HoodKickerConstants.HOOD_MOTOR_ID, CANBus.roboRIO());
-    hoodAppliedVolts = hood.getMotorVoltage();
-
     var hoodConfig = new TalonFXConfiguration();
     hoodConfig.CurrentLimits.SupplyCurrentLimit = HoodKickerConstants.HOOD_MOTOR_CURRENT_LIMIT;
     hoodConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
@@ -35,7 +47,6 @@ public class HoodKickerIOTalonFX implements HoodKickerIO {
     slot0Configs.kP = ShooterConstants.kP;
     slot0Configs.kI = ShooterConstants.kI;
     slot0Configs.kD = ShooterConstants.kD;
-
     hood.getConfigurator().apply(slot0Configs);
 
     var kickerConfig = new TalonFXConfiguration();
@@ -45,14 +56,47 @@ public class HoodKickerIOTalonFX implements HoodKickerIO {
     kickerConfig.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
     kicker.getConfigurator().apply(kickerConfig, 0.25);
 
-    // System.out.println(hood.getDescription());
+    BaseStatusSignal.setUpdateFrequencyForAll(
+        50,
+        hoodAppliedVolts,
+        hoodVelocity,
+        hoodPosition,
+        hoodTorqueCurrent,
+        hoodSupplyCurrent,
+        kickerAppliedVolts,
+        kickerVelocity,
+        kickerSupplyCurrent,
+        kickerTorqueCurrent);
 
+    kicker.optimizeBusUtilization();
+    hood.optimizeBusUtilization();
+
+    PhoenixUtil.registerSignals(
+        false,
+        hoodAppliedVolts,
+        hoodVelocity,
+        hoodPosition,
+        hoodTorqueCurrent,
+        hoodSupplyCurrent,
+        kickerAppliedVolts,
+        kickerVelocity,
+        kickerSupplyCurrent,
+        kickerTorqueCurrent);
   }
 
   public void updateInputs(HoodKickerIO.HoodIOInputs inputs) {
+    // Hood
     inputs.hoodAppliedVolts = hoodAppliedVolts.getValueAsDouble();
-    inputs.hoodPosition = hood.getPosition().getValueAsDouble();
-    inputs.kickerAppliedVolts = kicker.getMotorVoltage().getValueAsDouble();
+    inputs.hoodVelocity = hoodVelocity.getValueAsDouble();
+    inputs.hoodPosition = hoodPosition.getValueAsDouble();
+    inputs.hoodTorqueCurrent = hoodTorqueCurrent.getValueAsDouble();
+    inputs.hoodSupplyCurrent = hoodSupplyCurrent.getValueAsDouble();
+
+    // Kicker
+    inputs.kickerAppliedVolts = kickerAppliedVolts.getValueAsDouble();
+    inputs.kickerVelocity = kickerVelocity.getValueAsDouble();
+    inputs.kickerTorqueCurrent = kickerTorqueCurrent.getValueAsDouble();
+    inputs.kickerSupplyCurrent = kickerSupplyCurrent.getValueAsDouble();
   }
 
   public void setHoodPosition(double ticks) {
